02137e3b15508735a5e554741b5906c3a1e1a792
Silver Pisces <mediocre.judge2000@gmail.com>
2023-05-23 01:00:41 +0600

    Minor_Changes_Admin_Login

--- Admin_login.php
+++ Admin_login.php
@@ -12,104 +12,59 @@ require 'dbcon.php';
 	<title>Login</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
-	<style>
-		* {
-			box-sizing: border-box;
-			font-family: 'Montserrat', sans-serif;
-			margin: 0;
-			padding: 0;
-		}
-
-		body {
-			background-color: #f5f5f5;
-		}
-
-		.container {
-			background-color: #fff;
-			border-radius: 10px;
-			box-shadow: 0 0 10px rgba(0,0,0,0.2);
-			margin: 50px auto;
-			max-width: 500px;
-			padding: 30px;
-			text-align: center;
-		}
-		button{
-			background-color: white;
-			color: black;
-			font-size: large;
-			border: 1px solid green;
-			padding:8px;
-			padding-left: 30px;
-			padding-right: 30px;
-			border-radius: 5px;
-		}
-	button:hover {
-  background-color: green; /* Green */
-  color: white;
-}
-
-h1 {
-			font-size: 36px;
-			margin-bottom: 30px;
-			text-transform: uppercase;
-  background-image: linear-gradient(
-    -225deg,
-    #231557 0%,
-    #44107a 29%,
-    #ff1361 67%,
-    #fff800 100%);
-	  background-size: auto auto;
-  background-clip: border-box;
-  background-size: 200% auto;
-  color: #fff;
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  /* animation: textclip 2s linear infinite; */
-  display: inline-block;
-	  
-	  
-		}
-
-		form input {
-			background-color: #f5f5f5;
-			border: none;
-			border-radius: 5px;
-			color: #333;
-			font-size: 18px;
-			margin-bottom: 20px;
-			padding: 15px;
-			width: 100%;
-		}
-
-		form input[type="submit"] {
-			background-color: #008CBA;
-			color: #fff;
-			cursor: pointer;
-			font-weight: bold;
-			transition: background-color 0.2s ease-in-out;
-		}
-
-		form input[type="submit"]:hover {
-			background-color: #006F8F;
-		}
-
-		@media screen and (max-width: 600px) {
-			.container {
-				margin: 20px auto;
-				padding: 20px;
-			}
-		}
-	</style>
+	<?php logSign(); ?>
+	
 </head>
 <body>
-	<div class="container">
-		<h1>Admin Login</h1>
+
+<div class="text">
+		<p>
+		<h1>Find The Ideal Employees <br>That You Have Been Looking For</h1>
+		</p>
+	</div>
+
+
+	<div class="login-box">
+		<h2>Admin Login</h2>
 		<form action="code.php" method="POST">
-			<input type="text" name="User_Name" placeholder="Username">
-			<input type="password" name="Password" placeholder="Password">
-			<button type="submit"  name="adminLogin" class="btn btn-primary">Login</button>
+
+
+
+		  <div class="user-box">
+				<input type="text" name="User_Name" required="">
+				<label>Username</label>
+			</div>
+
+				<div class="user-box">
+				<input type="password" name="Password" required="">
+				<label>Password</label>
+			    </div>
+
+
+				<button type="submit"  name="adminLogin" class="btn btn-primary"><span></span>
+				<span></span>
+				<span></span>
+				<span></span>Login
+			</button>
 		</form>
+		<div class="forgot-password">
+			<a href="index.php">
+				<h6>Back To Home Page</h6>
+			</a>
+		</div>
+	</div>
+
+	
+
+	<div class="animation-area">
+		<ul class="box-area">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
 	</div>
 </body>
 </html>
